0115afe38218c59c9554154589374806db93534a
random1st <voroninr@gmail.com>
2026-09-09 15:33:45 +0100

    Release 0.3.6

--- Casks/feynt.rb
+++ Casks/feynt.rb
@@ -1,6 +1,6 @@
 cask "feynt" do
-  version "0.3.5"
-  sha256 "0d14ab5f2bed2d1f9dd6e52b04257bc1a4645696fdd085515e4d3d834ad8607e"
+  version "0.3.6"
+  sha256 "86ed628dcddac71d4d2d93dac3a22812af0ab1d14845ea7f43e444c2c5659ede"
 
   url "https://github.com/random1st/feynt/releases/download/v#{version}/Feynt-#{version}.dmg"
   name "Feynt"
